ac30cf1d47c04221bd375c81e52e9550f27f1966
eaviloria <eaviloria@sura.com.co>
2024-10-29 15:26:38 -0500

    src

---
+++ src/components/Results.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+
+export const Results = () => {
+  const [locations, setLocations] = useState([])
+  useEffect(() => {
+    fetch('https://6622071827fcd16fa6c8818c.mockapi.io/api/v1/blogs')
+    .then(response => response.json())
+    .then(results => setLocations(results))
+  }, [])
+
+  return (
+    <main className="results">
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Ubicacion</th>
+            <th>Reseña</th>
+            <th>Calificación</th>
+          </tr>
+        </thead>
+        <tbody>
+         {locations.forEach(location => {
+          <tr>
+              <td>{location.name}</td>
+              <td>{location.location}</td>
+              <td>{location.review}</td>
+              <td>{location.rating}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </main>
+  )
+}
